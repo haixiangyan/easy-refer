2655668374b552a686828bc8f61f8eccf597bd6b
Haixiang <haixiang6123@gmail.com>
2020-03-23 13:38:01 -0700

    完成菜单栏

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Public</router-link>|
-            <router-link to="/application">Application</router-link>|
-            <router-link to="/job">Job</router-link>|
-            <router-link to="/user">User</router-link>
-        </div>
-        <router-view/>
+        <Menu mode="horizontal" active-name="1">
+            <MenuItem class="logo-menu-item" name="0" to="/">
+                <img class="logo" src="./assets/img/logo.png" alt="">
+            </MenuItem>
+            <MenuItem name="1" to="/public">
+                内推广场
+            </MenuItem>
+            <MenuItem name="2" to="/application">
+                内推申请
+            </MenuItem>
+            <MenuItem name="3" to="/job">
+                发布职位
+            </MenuItem>
+            <MenuItem name="4" to="/user">
+                个人信息
+            </MenuItem>
+        </Menu>
+        <main>
+            <router-view/>
+        </main>
     </div>
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+    import Vue from 'vue'
+    import {Component} from 'vue-property-decorator'
+
+    @Component({
+      components: {}
+    })
+    export default class App extends Vue {
+
+    }
+</script>
+
+<style lang="scss">
+    a.logo-menu-item {
+        display: flex;
+        align-items: center;
+        .logo {
+            height: 40px;
+            width: 40px;
+        }
+    }
+</style>
