f1e291bdbb8232d6a36fadf04de4206a106fc5cb
Haixiang <haixiang6123@gmail.com>
2020-03-24 11:18:48 -0700

    伸缩菜单宽度

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
     <el-row class="app">
-        <el-col class="pane" :span="3">
+        <el-col class="pane" :span="menuWidth">
             <Menu v-if="$route.name !== 'Login'"/>
         </el-col>
-        <el-col class="pane" :span="21">
+        <el-col class="pane" :span="24 - menuWidth">
             <router-view/>
         </el-col>
     </el-row>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import {Component} from 'vue-property-decorator'
-    import Menu from "@/components/Menu.vue"
+  import Vue from "vue"
+  import {Component} from "vue-property-decorator"
+  import Menu from "@/components/Menu.vue"
 
-    @Component({
-      components: {Menu},
-    })
-    export default class App extends Vue {
+  @Component({
+    components: {Menu},
+    computed: {
+      menuWidth() {
+        return this.$route.name === "Login" ? 0 : 3
+      }
     }
+  })
+  export default class App extends Vue {
+  }
 </script>
 
 <style lang="scss">
     .app {
         height: 100vh;
+
         .pane {
             height: 100%;
         }
