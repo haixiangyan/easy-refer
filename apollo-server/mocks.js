32d21039f93889f82b15bd044e41e4c266d238ad
Haixiang <haixiang6123@gmail.com>
2020-03-29 11:40:09 -0700

    添加 faker

--- apollo-server/mocks.js
+++ apollo-server/mocks.js
@@ -1,10 +1,8 @@
 // Enable mocking in vue.config.js with `"pluginOptions": { "enableMocks": true }`
 // Customize mocking: https://www.apollographql.com/docs/graphql-tools/mocking.html#Customizing-mocks
+import faker from 'faker'
+
 export default {
     // Mock resolvers here
-    String: () => 'fuck',
-    User: () => ({
-        userId: '1',
-        name: 'Jack'
-    })
+    String: () => faker.name.findName()
 }
