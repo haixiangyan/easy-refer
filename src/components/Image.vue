1cc5021c5848bf7877d566791f187de3ed7d098a
Haixiang <haixiang6123@gmail.com>
2020-03-31 11:14:45 -0700

    使用 placeholder 图片

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,13 +1,10 @@
 <template>
     <el-image :lazy="lazy" :src="src" class="image" fit="cover">
         <div slot="error" class="image-slot">
-            <img style="width: 100%;" :src="errorSrc" alt="error">
+            <img :src="errorSrc" alt="error">
         </div>
         <div slot="placeholder" class="image-slot">
-            <p>
-                <i class="image-loading-icon el-icon-picture-outline"></i>
-            </p>
-            <p class="image-text">加载中...</p>
+            <img :src="placeholderSrc" alt="placeholder">
         </div>
     </el-image>
 </template>
@@ -21,26 +18,15 @@
     @Prop(Boolean) lazy = false
     @Prop({required: true, type: String}) src!: string
     @Prop(String) errorSrc: string = 'https://dummyimage.com/420x320/ff7f7f/333333.png&text=无'
+    @Prop(String) placeholderSrc: string = 'https://dummyimage.com/420x320/ff7f7f/333333.png&text=等'
   }
 </script>
 
 <style scoped lang="scss">
 .image {
     width: 80%;
-    background: #f5f7fa;
-    &-slot {
-        height: 140px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    &-loading-icon {
-        font-size: 1.5em;
-        color: #c0c4cc;
-    }
-    &-text {
-        color: #c0c4cc;
+    img {
+        width: 100%;
     }
 }
 </style>
