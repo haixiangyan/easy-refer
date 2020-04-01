6959c2a9bb3e3520ab8cc2252442264c0f743f43
Haixiang <haixiang6123@gmail.com>
2020-04-01 13:49:53 -0700

    添加 graphql-iso-date 包

--- apollo-server/resolvers.js
+++ apollo-server/resolvers.js
@@ -1,7 +1,9 @@
 import GraphQLJSON from 'graphql-type-json'
+import {GraphQLDateTime} from "graphql-iso-date/dist"
 
 export default {
     JSON: GraphQLJSON,
+    Date: GraphQLDateTime,
 
     Query: {},
 
