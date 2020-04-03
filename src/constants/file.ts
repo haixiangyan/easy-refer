0d77f440d447818979af425a67acceb0ee65fd7b
Haixiang <haixiang6123@gmail.com>
2020-04-02 19:23:26 -0700

    添加上传接口

---
+++ src/constants/file.ts
@@ -0,0 +1,2 @@
+export const IMAGE_SIZE = 2 * 1024 * 1024 // 2MB
+export const IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif']
--- src/views/EditUser.vue
+++ src/views/EditUser.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="edit-user" v-loading="loading" :element-loading-text="loadingText">
-        <el-row type="flex" align="middle">
+        <el-row class="avatar" type="flex" align="middle">
             <el-col :span="6">
-                <el-avatar class="avatar" :src="form ? form.avatarUrl : ''" :size="100"/>
+                <el-avatar :src="avatarUrl" :size="100"/>
             </el-col>
             <el-col>
-                <el-button size="small" type="success" plain round>修改头像</el-button>
+                <el-upload
+                    action=""
+                    :show-file-list="false"
+                    :on-success="onUpload"
+                    :before-upload="beforeUpload">
+                    <el-button size="small" type="success" plain round>修改头像</el-button>
+                </el-upload>
             </el-col>
         </el-row>
         <div v-if="form">
@@ -21,6 +27,9 @@
   import GetUserGQL from '@/graphql/GetUser.graphql'
   import {EDIT_USER_RULES} from "@/constants/rules"
   import EditUserForm from "@/components/EditUserForm.vue"
+  import {IMAGE_MIME_TYPES, IMAGE_SIZE} from '@/constants/file'
+  import UploadAvatarGQL from '@/graphql/UploadAvatar.graphql'
+  import {ElUploadInternalFileDetail} from 'element-ui/types/upload'
 
   @Component({
     components: {EditUserForm}
@@ -30,6 +39,7 @@
     rules = EDIT_USER_RULES
     loading = false
     loadingText = '加载中'
+    avatarUrl = ''
 
     get userId() {
       return this.$store.state.user.userId
@@ -37,6 +47,37 @@
 
     mounted() {
       this.loadUser()
+    }
+
+    async onUpload(res: {id: string}, file: ElUploadInternalFileDetail) {
+      try {
+        await this.$apollo.mutate({
+          mutation: UploadAvatarGQL,
+          variables: {userId: this.userId, avatar: file.raw}
+        })
+        this.avatarUrl = URL.createObjectURL(file.raw);
+        this.$message.success('修改成功')
+      } catch (error) {
+        this.$message.error(error.message)
+      } finally {
+        this.loading = false
+      }
+    }
+    beforeUpload(file: File) {
+      this.loading = true
+      this.loadingText = '上传中'
+
+      const isImage = IMAGE_MIME_TYPES.includes(file.type)
+      const isValidSize = file.size <= IMAGE_SIZE
+
+      if (!isImage) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isValidSize) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+
+      return isImage && isValidSize
     }
 
     async loadUser() {
@@ -49,6 +90,7 @@
         })
 
         this.form = data.user
+        this.avatarUrl = data.user.avatarUrl
       } catch (error) {
         this.$message.error(error.message)
       } finally {
@@ -79,4 +121,8 @@
   }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+    .avatar {
+        margin-bottom: 24px;
+    }
+</style>
