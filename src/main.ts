154566f565971edeacfff2d68094d302deec4128
Haixiang <haixiang6123@gmail.com>
2020-04-05 01:31:33 -0700

    完全清除 apollo 和 graphql

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
-import { createProvider } from './vue-apollo'
 import './plugins/element'
 import './registerServiceWorker'
 
@@ -19,6 +18,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
