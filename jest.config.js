7198f9984466b0c14138bb618921a8eaee778e30
Haixiang <haixiang6123@gmail.com>
2020-04-07 15:41:47 -0700

    添加测试覆盖率

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,28 @@
 module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
-  },
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'vue',
-    'json'
-  ],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ]
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+        '^~/(.*)$': '<rootDir>/$1',
+        '^vue$': 'vue/dist/vue.common.js'
+    },
+    moduleFileExtensions: [
+        'ts',
+        'js',
+        'vue',
+        'json'
+    ],
+    transform: {
+        "^.+\\.ts$": "ts-jest",
+        '^.+\\.js$': 'babel-jest',
+        '.*\\.(vue)$': 'vue-jest'
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        // 暂时不测试 components 和 pages 里的组件
+        // '<rootDir>/components/**/*.vue',
+        // '<rootDir>/pages/**/*.vue',
+        '<rootDir>/constants/**/*.ts',
+        '<rootDir>/utils/**/*.ts',
+        "!**/node_modules/**"
+    ],
+    coverageReporters: ["html", "text-summary", "lcov"],
 }
