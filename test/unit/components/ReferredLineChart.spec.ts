89eef6488a8be78ab72b76f06d0d27e09f72a255
Haixiang <haixiang6123@gmail.com>
2020-04-09 11:09:44 -0700

    添加 ReferredLineChart 单元测试

---
+++ test/unit/components/ReferredLineChart.spec.ts
@@ -0,0 +1,18 @@
+import {mount} from '@vue/test-utils'
+import ReferredLineChart from '@/components/ReferredLineChart.vue'
+
+describe('ReferredLineChart', () => {
+  it('正常显示', () => {
+    const dataSource = [{
+        date: '2019-01-01',
+        count: 100
+      }]
+
+    const wrapper = mount(ReferredLineChart, {
+      propsData: {dataSource},
+      stubs: ['ve-line']
+    })
+
+    expect(wrapper.exists()).toBe(true)
+  })
+})
