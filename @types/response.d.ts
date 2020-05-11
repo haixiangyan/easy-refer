780aff813a5a4a52ff861765e6e2820d113a5678
Haixiang <haixiang6123@gmail.com>
2020-05-11 12:30:02 -0700

    添加 auth type

--- @types/response.d.ts
+++ @types/response.d.ts
@@ -3,19 +3,3 @@ interface IUploadResume {
   url: string
   name: string
 }
-
-type TLoginForm = {
-  email: string
-  password: string
-}
-
-type TRegistrationForm = {
-  email: string
-  password: string
-  confirmPassword: string
-}
-
-type TActivationForm = {
-  email: string
-  password: string
-}
